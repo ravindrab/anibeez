443014efa14f1ebf2d7eb2256d69359b00602562
ravindrab <ravindra@anirasolutions.net>
2012-06-25 17:52:33 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anibeez::Application.config.secret_token = '71fc60b1c62f58468dad9a70f45260da544c0fc913005a9aff420cabb9b57f9feddf8d24f5b579956daaf8f9061036cba9c1934fcae658792fba0449044c0c7a'
