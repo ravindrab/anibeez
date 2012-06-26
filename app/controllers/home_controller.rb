aad468a2616478082fb82f5d02ce22395f277632
sandeep <sandeepb@anirasolutions.net>
2012-06-26 14:56:37 +0530

    gem devise installed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
-  def index
+  before_filter :authenticate_user!, :only => :index
+def index
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Anibeez::Application.routes.draw do
+  devise_for :users
+
   get "home/index"
   root :to => 'home#index'
 
