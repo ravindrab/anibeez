c1e7d8ed92baed34c09152fb9155c683f4cfe46f
ravindrab <ravindra@anirasolutions.net>
2012-07-06 12:49:45 +0530

    links index modified

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,6 @@
 class Link < ActiveRecord::Base
   belongs_to :bookmark
-  validates_format_of :url, :with => URI::regexp(%w(http https))
-  
+  validates_format_of :url, :with => URI::regexp(%w(http https www))
+  validates_length_of :url, :in => 7..32, :allow_blank => true 
   validates :url, :uniqueness => true
 end
