272e5a5dfa516d3faa4506b2bd7e8d03d184db67
ravindrab <ravindra@anirasolutions.net>
2012-07-07 13:35:36 +0530

    links validations commited

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,8 @@
 class Link < ActiveRecord::Base
   belongs_to :bookmark
-  validates_format_of :url, :with => URI::regexp(%w(http https www))
-  validates_length_of :url, :in => 7..32, :allow_blank => true 
+  #validates_format_of :url, :with => URI::regexp(%w(http|https ))
+  validates :url, :description, :presence => {:message => ' cannot be blank.'}, :allow_blank => true, :length => {:minimum => 2, :maximum => 40}
+
+  validates :url, :format =>{ :with => /^((http|https):\/\/)?[a-z0-9]+([-.]{1}[a-z0-9]+).[a-z]{2,5}(:[0-9]{1,5})?(\/.)?$/ix, :message => " is not valid" }
   validates :url, :uniqueness => true
 end
