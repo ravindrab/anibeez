5f8be26ed15dc57a4f44034d854ebab027a20b43
sandeep <sandeepm@anirasolutions.net>
2012-07-10 16:56:27 +0530

    dasdasdas

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,9 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!, :only => :index
 def index
  @user=current_user
+ @search = User.search(params[:search])
+ @users = @search.paginate :per_page => 20, :page => params[:page]
+
   end
 
 end
