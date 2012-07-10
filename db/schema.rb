279692ca79bed3a25d9438b8814814075395cd87
sandeep <sandeepb@anirasolutions.net>
2012-07-10 12:55:05 +0530

    paper clip updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120705060330) do
+ActiveRecord::Schema.define(:version => 20120710064633) do
 
   create_table "activities", :force => true do |t|
     t.text     "query"
@@ -75,6 +75,10 @@ ActiveRecord::Schema.define(:version => 20120705060330) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
